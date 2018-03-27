3a726d9a48d3668969acfe17172d05d0c46c8a01
Unknown <vincent1906@live.nl>
2018-03-27 11:59:45 +0200

    Boi hier komt die email

--- application/views/clients/edit.php
+++ application/views/clients/edit.php
@@ -9,7 +9,7 @@
 
 <label for="client_firstname">Voornaam</label><input  name="client_firstname" value = <?php echo $editContent['client_firstname'];?>><br/>
 <label for="client_lastname">Achternaam</label><input type="text" name="client_lastname" value = <?php echo $editContent['client_lastname'];?>><br/>
-<label for="client_email">Email</label><input type="text" name="client_email" value = <?php echo $editContent['client_email'];?>><br/>
+<label for="client_email">Email</label><input type="email" name="client_email" value = <?php echo $editContent['client_email'];?>><br/>
 <label for="client_phone">phone</label><input type="tel" name="client_phone" value = <?php echo $editContent['client_phone'];?>><br/>
 <input type="submit" name="submit" value="Edit a client"/>
 
